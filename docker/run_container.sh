55cb512e5f6ed81c9be42310e2c5fcf48ac9fee0
Jianqiao94 <118803075+Jianqiao94@users.noreply.github.com>
2024-07-26 17:16:38 +0200

    add GUI permission to run_container

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+xhost +local:root
+
+# Run the Docker container with the appropriate options
 docker run -it \
-           --rm \
-           -v "$(pwd)/../ros2_ws":/home/ros2_user/ros2_ws \
-           --env="DISPLAY" \
-           --env="QT_X11_NO_MITSHM=1" \
-           --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-           --name="ros2_container" \
-           --network=host \
-           ros2_image bash
+    --rm \
+    -v "$(pwd)/../ros2_ws":/home/ros2_user/ros2_ws \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    $DEVICE_OPTION \
+    --name="ros2_container" \
+    --network=host \
+    ros2_humble bash
 
 
 # if you need access to usb port with your container, add the follow line and change the port name
